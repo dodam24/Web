c0be0e3f4fa227ac2bb1482591f66b99247f5777
dodam24 <dodam0724@gmail.com>
2023-04-02 22:50:45 +0900

    myBatis

---
+++ mybatisTest/src/user/dao/UserDAO.java
@@ -0,0 +1,81 @@
+package user.dao;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import user.bean.UserDTO;
+
+public class UserDAO {
+	private SqlSessionFactory sqlSessionFactory;
+	private static UserDAO userDAO = new UserDAO();
+	
+	public static UserDAO getInstance() {
+		return userDAO;
+	}
+	
+	public UserDAO() {
+		//문자 단위
+		//Reader reader = Resources.getResourceAsReader("mybatis-config.xml"); //Reader는 추상클래스
+		
+		//byte단위
+		InputStream inputStream;
+		try {
+		inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+		
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		} catch(IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void write(UserDTO userDTO) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(); //생성
+		sqlSession.insert("userSQL.write", userDTO);
+		sqlSession.commit();
+		sqlSession.close();
+	}
+	
+	public List<UserDTO> getUserList(){
+		SqlSession sqlSession = sqlSessionFactory.openSession(); //생성
+		List<UserDTO> list = sqlSession.selectList("userSQL.getUserList");
+		sqlSession.close();
+		return list;
+	}
+	
+	public UserDTO getUser(String id) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(); //생성
+		UserDTO userDTO = sqlSession.selectOne("userSQL.getUser", id); //selectOne메소드는 오직 하나의 객체만 리턴한다(userMapper.xml로)
+		sqlSession.close();
+		return userDTO;
+	}
+	
+	public void update(Map<String, String> map) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(); //생성
+		sqlSession.update("userSQL.update", map);
+		sqlSession.commit();
+		sqlSession.close();
+	}
+	
+	public void delete(String id) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(); //생성
+		sqlSession.delete("userSQL.delete", id);
+		sqlSession.commit();
+		sqlSession.close();
+	}
+	
+	public List<UserDTO> search(Map<String, String> map) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(); //생성
+		List<UserDTO> list = sqlSession.selectList("userSQL.search", map);
+		sqlSession.close();
+		return list;
+	}
+	
+}
---
+++ mybatisTest/src/user/service/UserSearchService.java
@@ -0,0 +1,71 @@
+package user.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import user.bean.UserDTO;
+import user.dao.UserDAO;
+
+public class UserSearchService implements UserService {
+
+	@Override
+	public void execute() {
+		System.out.println();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("1. 이름 검색");
+		System.out.println("2. 아이디 검색");
+		System.out.print("번호 입력 : ");
+		int num = scan.nextInt();
+		
+		System.out.println();
+		String columnName = null;
+		String value = null;
+		if(num == 1) {
+			System.out.print("검색할 이름 입력 : ");
+			value = scan.next();
+			columnName = "name";
+		}else if(num == 2) {
+			System.out.println("검색할 아이디 입력 : ");
+			value = scan.next();
+			columnName = "id";
+		}
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("columnName", columnName);
+		map.put("value", value);
+		
+		//DB
+		UserDAO userDAO = UserDAO.getInstance();
+		List<UserDTO> list = userDAO.search(map);
+		
+		System.out.println("이름\t아이디\t비밀번호");
+		for(UserDTO userDTO : list) {
+			System.out.println(userDTO.getName()+"\t"+userDTO.getId()+"\t"+userDTO.getPwd());
+		}//for
+	}
+}
+
+/*
+	1. 이름 검색
+	2. 아이디 검색
+	번호 입력 : 1
+	
+	1번인 경우
+	찾고자 하는 이름 입력 : 홍
+	
+	이름      아이디      비밀번호
+	홍길동
+	홍당무
+	
+	1번인 경우
+	찾고자 하는 아이디 입력 : n
+	
+	이름      아이디      비밀번호
+	      	hong
+	      	conan
+	      
+	이름으로 검색하건 또는 아이디로 검색하건 무조건 userDAO.search(~~)를 호출한다.
+*/
